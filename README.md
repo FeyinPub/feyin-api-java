239a456f9f770a31ab7395f09d29e1a16b5bf355
Tom <tomas1221@163.com>
2017-11-14 14:40:52 +0800

    update readme to add some example

--- README.md
+++ README.md
@@ -6,5 +6,25 @@
 
 ## 使用方法
 
+### maven 依赖包
 
+```xml
+<dependency>
+    <groupId>net.feyin.api</groupId>
+    <artifactId>feyin-api</artifactId>
+    <version>1.0.2</version>
+</dependency>
+```
 
+### code
+
+```java
+String memberCode="fb58abf0eb8111e68c6c52540008b6e6";
+String apiKey = "32ab6d12";
+FeyinClient feyinClient = new FeyinClient(memberCode, apiKey);
+FeyinMessage msg = new FeyinMessage(deviceNo, "需要打印的消息内容");
+int result = feyiniClient.sendFreeMessage(msg);
+if(result == 0){
+    System.out.println("打印成功");
+}
+```
--- src/main/java/com/feyin/api/FeyinClient.java
+++ src/main/java/com/feyin/api/FeyinClient.java
@@ -24,281 +24,278 @@ import com.feyin.util.MD5Ecnrypt;
 
 /**
  * 飞印api
- * 
+ *
  * @author tommy.zeng
- * 
  */
 public class FeyinClient {
 
-	String host = "my.feyin.net";
-	int port = 80;
-	String apiKey;  //api 密钥，由feyin网提供
-	String memberCode; //商户的代码，由feyin网提供
+    static final String FEYIN_HOST = "my.feyin.net";
+    static final int FEYIN_PORT = 80;
 
-	public FeyinClient(String host, int port, String apiKey, String memberCode) {
-		super();
-		this.host = host;
-		this.port = port;
-		this.apiKey = apiKey;
-		this.memberCode = memberCode;
-	}
+    final String host;
+    final int port;
+    String apiKey;  //api 密钥，由feyin网提供
+    String memberCode; //商户的代码，由feyin网提供
 
-	
+    public FeyinClient(String memberCode,String apiKey) {
+        this(FEYIN_HOST, FEYIN_PORT, apiKey, memberCode);
+    }
 
-	/**
-	 * 提交格式化的内容
-	 * @param msg
-	 * @return 详见api文档
-	 * @throws Exception
-	 */
-	public int sendFormatedMessage(FeyinMessage msg) throws Exception {
-		if (msg.getMessageType() != FeyinMessageType.formatedMessage)
-			throw new Exception("wrong message type");
+    public FeyinClient(String host, int port, String apiKey, String memberCode) {
+        super();
+        this.host = host;
+        this.port = port;
+        this.apiKey = apiKey;
+        this.memberCode = memberCode;
+    }
 
-		long reqTime = System.currentTimeMillis();
 
-		String contentToEncryt = String.format("%s%s%s%s%s%s%s%s%d%s",
-				this.memberCode, msg.customerName, msg.customerPhone,
-				msg.customerAddress, msg.customerMemo, msg.msgDetail,
-				msg.deviceNo, msg.msgNo, reqTime, this.apiKey);
-	
-		String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
+    public int sendMsg(String deviceNo, String msgDetail) throws Exception{
+        return sendFreeMessage(new FeyinMessage(FeyinMessageType.freeMessage, this.memberCode, deviceNo, msgDetail));
+    }
 
-		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+    /**
+     * 提交格式化的内容
+     *
+     * @param msg
+     * @return 详见api文档
+     * @throws Exception
+     */
+    public int sendFormatedMessage(FeyinMessage msg) throws Exception {
+        if (msg.getMessageType() != FeyinMessageType.formatedMessage)
+            throw new Exception("wrong message type");
 
-		nvps.add(new BasicNameValuePair("memberCode", memberCode));
-		nvps.add(new BasicNameValuePair("charge", msg.charge));
-		nvps.add(new BasicNameValuePair("customerName", msg.customerName));
-		nvps.add(new BasicNameValuePair("customerPhone", msg.customerPhone));
-		nvps.add(new BasicNameValuePair("customerAddress", msg.customerAddress));
-		nvps.add(new BasicNameValuePair("customerMemo", msg.customerMemo));
-		nvps.add(new BasicNameValuePair("msgDetail", msg.msgDetail));
-		nvps.add(new BasicNameValuePair("deviceNo", msg.deviceNo));
-		nvps.add(new BasicNameValuePair("msgNo", msg.msgNo));
-		
-		nvps.add(new BasicNameValuePair("extra1", msg.extra1));
-		nvps.add(new BasicNameValuePair("extra2", msg.extra2));
-		nvps.add(new BasicNameValuePair("extra3", msg.extra3));
-		nvps.add(new BasicNameValuePair("extra4", msg.extra4));
-		
-		nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
+        long reqTime = System.currentTimeMillis();
 
-		nvps.add(new BasicNameValuePair("securityCode", securityCode));
-		nvps.add(new BasicNameValuePair("mode", "1"));
+        String contentToEncryt = String.format("%s%s%s%s%s%s%s%s%d%s",
+                this.memberCode, msg.customerName, msg.customerPhone,
+                msg.customerAddress, msg.customerMemo, msg.msgDetail,
+                msg.deviceNo, msg.msgNo, reqTime, this.apiKey);
 
-		return sendMessage(nvps);
+        String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
 
-	}
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 
-	/**
-	 * 提交自由格式的内容
-	 * @param msg
-	 * @return 详见api文档
-	 * @throws Exception
-	 */
-	public int sendFreeMessage(FeyinMessage msg) throws Exception {
+        nvps.add(new BasicNameValuePair("memberCode", memberCode));
+        nvps.add(new BasicNameValuePair("charge", msg.charge));
+        nvps.add(new BasicNameValuePair("customerName", msg.customerName));
+        nvps.add(new BasicNameValuePair("customerPhone", msg.customerPhone));
+        nvps.add(new BasicNameValuePair("customerAddress", msg.customerAddress));
+        nvps.add(new BasicNameValuePair("customerMemo", msg.customerMemo));
+        nvps.add(new BasicNameValuePair("msgDetail", msg.msgDetail));
+        nvps.add(new BasicNameValuePair("deviceNo", msg.deviceNo));
+        nvps.add(new BasicNameValuePair("msgNo", msg.msgNo));
 
-		if (msg.getMessageType() != FeyinMessageType.freeMessage)
-			throw new Exception("wrong message type");
+        nvps.add(new BasicNameValuePair("extra1", msg.extra1));
+        nvps.add(new BasicNameValuePair("extra2", msg.extra2));
+        nvps.add(new BasicNameValuePair("extra3", msg.extra3));
+        nvps.add(new BasicNameValuePair("extra4", msg.extra4));
 
-		long reqTime = System.currentTimeMillis();
+        nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
 
-		String contentToEncryt = String.format("%s%s%s%s%d%s", this.memberCode,
-				msg.msgDetail, msg.deviceNo, msg.msgNo, reqTime, this.apiKey);
+        nvps.add(new BasicNameValuePair("securityCode", securityCode));
+        nvps.add(new BasicNameValuePair("mode", "1"));
 
-		String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
+        return sendMessage(nvps);
 
-		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+    }
 
-		nvps.add(new BasicNameValuePair("memberCode", memberCode));
-		nvps.add(new BasicNameValuePair("msgDetail", msg.msgDetail));
-		nvps.add(new BasicNameValuePair("deviceNo", msg.deviceNo));
-		nvps.add(new BasicNameValuePair("msgNo", msg.msgNo));
-		nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
-		
-		nvps.add(new BasicNameValuePair("securityCode", securityCode));
-		nvps.add(new BasicNameValuePair("mode", "2"));
+    /**
+     * 提交自由格式的内容
+     *
+     * @param msg
+     * @return 详见api文档
+     * @throws Exception
+     */
+    public int sendFreeMessage(FeyinMessage msg) throws Exception {
 
-		return sendMessage(nvps);
+        if (msg.getMessageType() != FeyinMessageType.freeMessage)
+            throw new Exception("wrong message type");
 
-	}
+        long reqTime = System.currentTimeMillis();
 
-	/**
-	 * 查询异常打印列表 
-	 * @return 详见api文档
-	 * @throws Exception
-	 */
-	public String listException() throws Exception {
-		long reqTime = System.currentTimeMillis();
-		String contentToEncryt = String.format("%s%d%s", this.memberCode,
-				reqTime, this.apiKey);
+        String contentToEncryt = String.format("%s%s%s%s%d%s", this.memberCode,
+                msg.msgDetail, msg.deviceNo, msg.msgNo, reqTime, this.apiKey);
 
-		String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
+        String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
 
-		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 
-		nvps.add(new BasicNameValuePair("memberCode", memberCode));
-		nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
+        nvps.add(new BasicNameValuePair("memberCode", memberCode));
+        nvps.add(new BasicNameValuePair("msgDetail", msg.msgDetail));
+        nvps.add(new BasicNameValuePair("deviceNo", msg.deviceNo));
+        nvps.add(new BasicNameValuePair("msgNo", msg.msgNo));
+        nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
 
-		nvps.add(new BasicNameValuePair("securityCode", securityCode));
+        nvps.add(new BasicNameValuePair("securityCode", securityCode));
+        nvps.add(new BasicNameValuePair("mode", "2"));
 
-		return sendGetMessage("/api/listException", nvps);
-	}
+        return sendMessage(nvps);
 
-	/**
-	 * 查询设备列表
-	 * @return 详见api文档
-	 * @throws Exception
-	 */
-	public String listDevice() throws Exception {
-		long reqTime = System.currentTimeMillis();
-		String contentToEncryt = String.format("%s%d%s", this.memberCode,
-				reqTime, this.apiKey);
+    }
 
-		String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
+    /**
+     * 查询异常打印列表
+     *
+     * @return 详见api文档
+     * @throws Exception
+     */
+    public String listException() throws Exception {
+        long reqTime = System.currentTimeMillis();
+        String contentToEncryt = String.format("%s%d%s", this.memberCode,
+                reqTime, this.apiKey);
 
-		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+        String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
 
-		nvps.add(new BasicNameValuePair("memberCode", memberCode));
-		nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 
-		nvps.add(new BasicNameValuePair("securityCode", securityCode));
+        nvps.add(new BasicNameValuePair("memberCode", memberCode));
+        nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
 
-		return sendGetMessage("/api/listDevice", nvps);
-	}
+        nvps.add(new BasicNameValuePair("securityCode", securityCode));
 
-	/**
-	 * 查询消息打印的状态
-	 * @param msgNo
-	 * @return  详见api文档
-	 * @throws Exception
-	 */
-	public int queryState(String msgNo) throws Exception {
-		long reqTime = System.currentTimeMillis();
-		String contentToEncryt = String.format("%s%d%s%s", this.memberCode,
-				reqTime, this.apiKey, msgNo);
+        return sendGetMessage("/api/listException", nvps);
+    }
 
-		String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
-		
-		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+    /**
+     * 查询设备列表
+     *
+     * @return 详见api文档
+     * @throws Exception
+     */
+    public String listDevice() throws Exception {
+        long reqTime = System.currentTimeMillis();
+        String contentToEncryt = String.format("%s%d%s", this.memberCode,
+                reqTime, this.apiKey);
 
-		nvps.add(new BasicNameValuePair("memberCode", memberCode));
-		nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
+        String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
 
-		nvps.add(new BasicNameValuePair("securityCode", securityCode));
-		nvps.add(new BasicNameValuePair("msgNo", msgNo));
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 
-		String result = sendGetMessage("/api/queryState", nvps);
+        nvps.add(new BasicNameValuePair("memberCode", memberCode));
+        nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
 
-		return Integer.parseInt(result);
-	}
+        nvps.add(new BasicNameValuePair("securityCode", securityCode));
 
-	/**
-	 * 公共的发送post请求的方法
-	 * @param nvps
-	 * @return
-	 * @throws Exception
-	 */
-	private int sendMessage(List<NameValuePair> nvps) throws Exception {
-		int result = 0;
-		HttpPost httppost = new HttpPost("http://" + this.host + ":"
-				+ this.port + "/api/sendMsg");
+        return sendGetMessage("/api/listDevice", nvps);
+    }
 
-		httppost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
-		HttpClient httpclient = new DefaultHttpClient();
+    /**
+     * 查询消息打印的状态
+     *
+     * @param msgNo
+     * @return 详见api文档
+     * @throws Exception
+     */
+    public int queryState(String msgNo) throws Exception {
+        long reqTime = System.currentTimeMillis();
+        String contentToEncryt = String.format("%s%d%s%s", this.memberCode,
+                reqTime, this.apiKey, msgNo);
 
-		HttpResponse response = httpclient.execute(httppost);
-		HttpEntity resEntity = response.getEntity();
-		int returnCode = response.getStatusLine().getStatusCode();
+        String securityCode = MD5Ecnrypt.EncodeMD5Hex(contentToEncryt);
 
-		if (returnCode == 200 || returnCode == 302) {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					resEntity.getContent()));
-			
-			String readLine;
-			if (((readLine = br.readLine()) != null)) {
-				result = Integer.parseInt(readLine);
-			}
-		} else {
-			result = -10;
-		}
-		
-		httpclient.getConnectionManager().shutdown();
+        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 
-		return result;
-	}
+        nvps.add(new BasicNameValuePair("memberCode", memberCode));
+        nvps.add(new BasicNameValuePair("reqTime", String.valueOf(reqTime)));
 
-	/**
-	 * 公共的发送Get请求的方法
-	 * @param url
-	 * @param nvps
-	 * @return
-	 * @throws Exception
-	 */
-	private String sendGetMessage(String url, List<NameValuePair> nvps)
-			throws Exception {
-		String result = "";
-		
-		URI uri = URIUtils.createURI("http", this.host, this.port, url, 
-				URLEncodedUtils.format(nvps, "UTF-8"), null);
-		
-		HttpGet httpget = new HttpGet(uri);
-		HttpClient httpclient = new DefaultHttpClient();
+        nvps.add(new BasicNameValuePair("securityCode", securityCode));
+        nvps.add(new BasicNameValuePair("msgNo", msgNo));
 
-		HttpResponse response = httpclient.execute(httpget);
-		HttpEntity resEntity = response.getEntity();
-		int returnCode = response.getStatusLine().getStatusCode();
+        String result = sendGetMessage("/api/queryState", nvps);
 
-		if (returnCode == 200 || returnCode == 302) {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					resEntity.getContent()));
-			
-			String readLine;
-			StringBuffer buf = new StringBuffer();
-			while (((readLine = br.readLine()) != null)) {
-				buf.append(readLine);
-			}
-			result = buf.toString();
-		} else {
-			result = "-10";
-		}
+        return Integer.parseInt(result);
+    }
 
-		httpclient.getConnectionManager().shutdown();
+    /**
+     * 公共的发送post请求的方法
+     *
+     * @param nvps
+     * @return
+     * @throws Exception
+     */
+    private int sendMessage(List<NameValuePair> nvps) throws Exception {
+        int result = 0;
+        HttpPost httppost = new HttpPost("http://" + this.host + ":"
+                + this.port + "/api/sendMsg");
 
-		return result;
+        httppost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+        HttpClient httpclient = new DefaultHttpClient();
 
-	}
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity resEntity = response.getEntity();
+        int returnCode = response.getStatusLine().getStatusCode();
 
-	
-	public String getHost() {
-		return host;
-	}
+        if (returnCode == 200 || returnCode == 302) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    resEntity.getContent()));
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+            String readLine;
+            if (((readLine = br.readLine()) != null)) {
+                result = Integer.parseInt(readLine);
+            }
+        } else {
+            result = -10;
+        }
 
-	public int getPort() {
-		return port;
-	}
+        httpclient.getConnectionManager().shutdown();
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+        return result;
+    }
 
-	public String getApiKey() {
-		return apiKey;
-	}
+    /**
+     * 公共的发送Get请求的方法
+     *
+     * @param url
+     * @param nvps
+     * @return
+     * @throws Exception
+     */
+    private String sendGetMessage(String url, List<NameValuePair> nvps)
+            throws Exception {
+        String result = "";
 
-	public void setApiKey(String apiKey) {
-		this.apiKey = apiKey;
-	}
+        URI uri = URIUtils.createURI("http", this.host, this.port, url,
+                URLEncodedUtils.format(nvps, "UTF-8"), null);
 
-	public String getMemberCode() {
-		return memberCode;
-	}
+        HttpGet httpget = new HttpGet(uri);
+        HttpClient httpclient = new DefaultHttpClient();
 
-	public void setMemberCode(String memberCode) {
-		this.memberCode = memberCode;
-	}
+        HttpResponse response = httpclient.execute(httpget);
+        HttpEntity resEntity = response.getEntity();
+        int returnCode = response.getStatusLine().getStatusCode();
+
+        if (returnCode == 200 || returnCode == 302) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    resEntity.getContent()));
+
+            String readLine;
+            StringBuffer buf = new StringBuffer();
+            while (((readLine = br.readLine()) != null)) {
+                buf.append(readLine);
+            }
+            result = buf.toString();
+        } else {
+            result = "-10";
+        }
+
+        httpclient.getConnectionManager().shutdown();
+
+        return result;
+
+    }
+
+
+    public String getHost() {
+        return host;
+    }
+    public int getPort() {
+        return port;
+    }
+    public String getApiKey() {
+        return apiKey;
+    }
+    public String getMemberCode() {
+        return memberCode;
+    }
 }
